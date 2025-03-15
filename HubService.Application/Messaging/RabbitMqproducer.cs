618acb09b422b3ebc54b2931aae5e6929ccda699
anrimichilashvili <anrimichilaschili21@gmail.com>
2025-03-15 20:18:20 +0400

    moved rabbitmq string into json

--- HubService.Application/Messaging/RabbitMqproducer.cs
+++ HubService.Application/Messaging/RabbitMqproducer.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
 using System.Text;
 using System.Text.Json;
 
@@ -7,16 +8,18 @@ namespace HubService.Application.Messaging
     public class RabbitMqproducer : IMessageProducer
     {
         private readonly IRabbitMqConnection _connection;
-        public RabbitMqproducer(IRabbitMqConnection connection)
+        private readonly IConfiguration _configuration;
+        public RabbitMqproducer(IRabbitMqConnection connection, IConfiguration configuration)
         {
             _connection = connection;
+            _configuration = configuration;
         }
 
         public void SendMessage<T>(T message)
         {
-            string exchangeName = "HubServiceExchange";
-            string routingKey = "HubService-routing-key";
-            string queueName = "HubService";
+            var exchangeName = _configuration["RabbitMq:ExchangeName"];
+            var routingKey = _configuration["RabbitMq:RoutingKey"];
+            var queueName = _configuration["RabbitMq:QueueName"];
 
             using var channel = _connection.Connection.CreateModel();
 
